4c9054b32a969478ea8ef0805e94078e77795ade
josator2 <josator2@gmail.com>
2022-10-05 15:00:38 +0200

    make protocols for RM (incomplete)

--- xnat2mids/mids_conversion.py
+++ xnat2mids/mids_conversion.py
@@ -3,21 +3,28 @@ import re
 from xnat2mids import io_json
 from xnat2mids.procedures.magnetic_resonance_procedures import ProceduresMR
 from xnat2mids.protocols.scans_tagger import Tagger
+
 subses_pattern = r"[A-z]+(?P<prefix_sub>\d*)?(_S)(?P<suffix_sub>\d+)/[A-z]+(?P<prefix_ses>\d*)?(_E)(?P<suffix_ses>\d+)"
-dict_keys = {'Modality':'00080060', 'Series Description':'0008103E', 'Protocol Name': '00181030'}
+dict_keys = {
+    'Modality': '00080060',
+    'Series Description': '0008103E',
+    'Protocol Name': '00181030',
+    'Complex Image Component Attribute': '00089208'}
+
 dict_mr_keys = {
     'Manufacturer': '00080070',
     'Scanning Sequence': '00180020',
     'SequenceVariant': '00180021',
     'ScanOptions': '00180022',
-	'Angio Flag' : '00180025',
+    'Angio Flag': '00180025',
     'MagneticFieldStrength': '00180087',
     'RepetitionTime': '00180080',
     'Inversion Time': '00180082',
     'FlipAngle': '00181314',
     'EchoTime': '00180081',
-    'SliceThickness': '00180050'
+    'SliceThickness': '00180050',
 }
+
 BIOFACE_PROTOCOL_NAMES = [
     '3D-T2-FLAIR SAG',
     '3D-T2-FLAIR SAG NUEVO-1',
@@ -25,19 +32,19 @@ BIOFACE_PROTOCOL_NAMES = [
     'ADVANCED_ASL',
     'AXIAL T2 TSE FS',
     'AX_T2_STAR',
-    'DTIep2d_diff_mddw_48dir_p3_AP',
-    'DTIep2d_diff_mddw_4b0_PA',
+    'DTIep2d_diff_mddw_48dir_p3_AP', #
+    'DTIep2d_diff_mddw_4b0_PA', #
     'EPAD-3D-SWI',
-    'EPAD-B0-RevPE',
+    'EPAD-B0-RevPE', # PA
     'EPAD-SE-fMRI',
     'EPAD-SE-fMRI-RevPE',
-    'EPAD-SingleShell-DTI48',
+    'EPAD-SingleShell-DTI48', # AP
     'EPAD-rsfMRI (Eyes Open)',
-    'MPRAGE_GRAPPA2',
+    'MPRAGE_GRAPPA2', # T1 mprage
     'asl_3d_tra_iso_3.0_highres',
     'pd+t2_tse_tra_p2_3mm',
-    't1_mprage_sag_p2_iso',
-    't2_space_dark-fluid_sag_p2_iso',
+    't1_mprage_sag_p2_iso', # t1
+    't2_space_dark-fluid_sag_p2_iso', # flair
     't2_swi_tra_p2_384_2mm'
 ]
 
@@ -51,6 +58,7 @@ BIOFACE_PROTOCOL_NAMES_DESCARTED = [
     'EPAD-rsfMRI (Eyes Open)',
     'EPAD-SE-fMRI-RevPE'
 ]
+
 
 def create_directory_mids_v1(xnat_data_path, mids_data_path, body_part):
     procedure_class_mr = ProceduresMR()
@@ -78,14 +86,14 @@ def create_directory_mids_v1(xnat_data_path, mids_data_path, body_part):
                 study_description = dict_json[dict_keys['Series Description']].get("Value", ["n/a"])[0]
                 if modality == "MR":
                     # via BIDS protocols
-                    if body_part in ["head","brain"]:
+                    if body_part in ["head", "brain"]:
                         ProtocolName = dict_json[dict_keys['Protocol Name']].get("Value", ["n/a"])[0]
                         if ProtocolName not in BIOFACE_PROTOCOL_NAMES_DESCARTED:
                             json_adquisitions = {
-                                f"{k} ('{v}')":dict_json[v].get("Value", ["n/a"])[0] for k,v in dict_mr_keys.items()
+                                f"{k} ('{v}')": dict_json[v].get("Value", ["n/a"])[0] for k, v in dict_mr_keys.items()
                             }
 
-                            protocol,acq,folder_BIDS = tagger.classification_by_min_max(json_adquisitions)
+                            protocol, acq, folder_BIDS = tagger.classification_by_min_max(json_adquisitions)
                             print(protocol, acq, folder_BIDS)
 
                             procedure_class_mr.convert_to_bids(
--- xnat2mids/procedures/magnetic_resonance_procedures.py
+++ xnat2mids/procedures/magnetic_resonance_procedures.py
@@ -1,38 +1,44 @@
 from xnat2mids.dicom_converters import dicom2nii
 
+body_part_bids = ['head','brain']
 
 class ProceduresMR:
+    def __init__(self):
+        self.reset_indexes()
 
     def reset_indexes(self):
         """Reset all values in dictionary of the atribute "dict_indexes"."""
 
-        self.dict_indexes_MIDS = {}
-        self.dict_indexes_BIDS = {}
+        self.run_dict = {}
 
 
-    def convert_to_bids(
+    def control_sequences(
         self, json_path, mids_session_path, protocol, acq, folder_BIDS, body_part
     ):
 
-        folder_to_image = mids_session_path.joinpath(folder_BIDS)
-        folder_to_image.mkdir(parents=True, exist_ok=True)
+        folder_image_mids = mids_session_path.joinpath(
+             "" if body_part in body_part_bids else "mim-mr",
+            folder_BIDS
+        )
+        folder_image_mids.mkdir(parents=True, exist_ok=True)
         nifti_path = dicom2nii(json_path)
 
         if folder_BIDS == "dwi":
             pass
         if folder_BIDS == "anat":
-            self.anatomic_procedure(nifti_path)
+            self.anatomic_procedure(nifti_path, folder_image_mids, protocol, acq, body_part)
         if folder_BIDS == "fmap":
             pass
         if folder_BIDS == "func":
             pass
         if folder_BIDS == "perf":
             pass
-    def anatomic_procedure(self, nifti_path):
+
+
+    def control_image(self, nifti_path,folder_image_mids, protocol, acq, body_part):
 
         """
-            Function that copies the elements to the anatomical folder of
-            BIDS/MIDS structure.
+
         """
 
 
@@ -43,9 +49,33 @@ class ProceduresMR:
         if len_nifti_files == 0: return
 
         # This is a counter for several nifties in one adquisition
-        for acq_index, nifti_file in enumerate(nifti_files, 1):
 
 
+        print("processing file -> ", nifti_files)
+        protocol_label = f'_{protocol}'
+        acq_label = f'_acq-{acq}' if acq else ''
+        bp_label = f"" if body_part in body_part_bids else f"_bp-{body_part}"
+        vp_label = [
+            (
+                f"_desc-{self.get_plane_nib(nifti_file)}"
+                if body_part in body_part_bids
+                else f"_vp-{self.get_plane_nib(nifti_file)}"
+            )
+            for nifti_file in nifti_files
+        ]
+        key = str([protocol_label, acq_label, bp_label, vp_label])
+        value = self.run_dict.get(key, []).append([nifti_files, folder_image_mids])
+        self.run_dict[key] = value
 
+    def get_plane_nib(nifti_list):
+        """
+            Calculate the type of plane with the tag image orientation patient
+            in dicom metadata.
+        """
 
+        img = nib.load(nifti)
+        plane = nib.aff2axcodes(img.affine)[2]
+        return "ax" if plane in ["S", "I"] else "sag" if plane in ["R", "L"] else "cor"
 
+    def copy_session(self):
+        pass
